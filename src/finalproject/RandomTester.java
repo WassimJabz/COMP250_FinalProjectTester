516a71b6c8baa0ee2fe6c92521ad6795f377832e
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-04-25 12:28:26 -0400

    Create 5x5 with knight+king+queen rules

--- src/finalproject/RandomTester.java
+++ src/finalproject/RandomTester.java
@@ -6,8 +6,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 public class RandomTester {
-	private static final double SEED_DENSITY = 0.05;
-	private static final double MIN_CLUE_DENSITY = 0.4;
+	private static final double SEED_DENSITY = 0.1;
+	private static final double MIN_CLUE_DENSITY = 0.5;
 	private static final double MAX_CLUE_DENSITY = 0.6;
 	private static final boolean SHOW_PROGRESS = true;
 	private static final long TIMEOUT_MILLIS = 60000;
