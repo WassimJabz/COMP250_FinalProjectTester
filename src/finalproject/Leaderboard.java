20f0bb432ba887986e8d7e1f51ec32984f006a3a
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-04-29 00:49:49 -0400

    Create leaderboard viewer

---
+++ src/finalproject/Leaderboard.java
@@ -0,0 +1,55 @@
+package finalproject;
+
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import comp250.leaderboard.LeaderboardReader;
+
+public class Leaderboard {
+
+	public static void main(String[] args) {
+
+		// Retrieve data
+		String responseBody;
+		try {
+			responseBody = LeaderboardReader.getTop20Results();
+		} catch (IOException e) {
+			System.out.println("The results could not be retrieved.");
+			return;
+		}
+
+		// Parse JSON
+		JSONArray array = new JSONArray(responseBody);
+		final int numResults = array.length();
+		String[] names = new String[numResults];
+		int[] scores = new int[numResults];
+		String[] descriptions = new String[numResults];
+		int maxNameLength = 0;
+		int maxScoreLength = 0;
+		for (int i = 0; i < numResults; i++) {
+			JSONObject obj = array.getJSONObject(i);
+
+			names[i] = obj.getString("name");
+			if (names[i].length() > maxNameLength)
+				maxNameLength = names[i].length();
+
+			scores[i] = obj.getInt("testScore");
+			int len = Integer.toString(scores[i]).length();
+			if (len > maxScoreLength)
+				maxScoreLength = len;
+
+			descriptions[i] = obj.getString("description");
+		}
+
+		// Display data
+		int scoreWidth = Math.max(maxScoreLength, 5);
+		System.out.printf("%s  %-" + maxNameLength + "s  %-" + scoreWidth
+				+ "s  %s\n", "Rank", "Name", "Score", "Description");
+		for (int rank = 0; rank < numResults; rank++) {
+			System.out.printf("%4d  %" + maxNameLength + "s  %" + scoreWidth
+					+ "d  %s\n", rank + 1, names[rank], scores[rank],
+					descriptions[rank]);
+		}
+	}
+}
